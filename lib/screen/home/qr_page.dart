26946558977f0b546f753ceabc4a2a83ce2606d7
Jenny <jenny.lynn.fletcher@gmail.com>
2021-01-23 19:48:16 +0000

    Read barcode/QR code and prints value and type to console

--- lib/screen/home/qr_page.dart
+++ lib/screen/home/qr_page.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
 /// Page to add new foods to the fridge.
 class QrPage extends StatefulWidget {
@@ -18,6 +19,7 @@ class QrPage extends StatefulWidget {
 class _QrState extends State<QrPage> {
   File _image;
   final picker = ImagePicker();
+  var text = '';
 
   Future getImage() async {
     final pickedFile = await picker.getImage(source: ImageSource.camera);
@@ -29,6 +31,32 @@ class _QrState extends State<QrPage> {
         print('No image selected.');
       }
     });
+
+    FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(_image);
+
+    final BarcodeDetector barcodeDetector = FirebaseVision.instance.barcodeDetector();
+
+    final List<Barcode> barcodes = await barcodeDetector.detectInImage(visionImage);
+
+    for (Barcode barcode in barcodes) {
+
+      final String rawValue = barcode.rawValue;
+      final BarcodeValueType valueType = barcode.valueType;
+
+      setState(() {
+        text ="$rawValue\nType: $valueType";
+        print(text);
+      });
+
+    }
+    if (barcodes.length == 0) {
+      setState(() {
+        text ='No barcode detected';
+        print(text);
+      });
+    }
+
+    barcodeDetector.close();
   }
 
   @override
