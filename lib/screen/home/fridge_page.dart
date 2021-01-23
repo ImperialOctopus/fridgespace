8775d8298e9a29dbdba3078db2d4b00c5e1b85d6
OliverWales <oliverfwales@gmail.com>
2021-01-23 20:49:05 +0000

    Fetch fridge items by UID

--- lib/screen/home/fridge_page.dart
+++ lib/screen/home/fridge_page.dart
@@ -1,4 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../bloc/authentication/authentication_bloc.dart';
+import '../../bloc/authentication/authentication_state.dart';
 
 /// Page to list items in the user's fridge.
 class FridgePage extends StatelessWidget {
@@ -7,6 +12,31 @@ class FridgePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Fridge'));
+    return BlocBuilder<AuthenticationBloc, AuthenticationState>(
+      builder: (context, state) {
+        User user;
+
+        if (state is UserAuthenticated) {
+          print(state.user);
+          user = state.user;
+        } else {
+          throw Error();
+        }
+
+        return FutureBuilder<QuerySnapshot>(
+          future: FirebaseFirestore.instance.collection(user.uid).get(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Column(
+                  children: snapshot.data.docs
+                      .map<Widget>((x) => Text(x.get('text').toString()))
+                      .toList());
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        );
+      },
+    );
   }
 }
