91a2c3969d894e11f461a576ccf93a8ab0462929
OliverWales <oliverfwales@gmail.com>
2021-01-23 21:30:56 +0000

    Implement FoodItem and FirebaseDatabaseRepository

---
+++ lib/model/food_item.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FoodItem {
+  const FoodItem({
+    @required this.name,
+  });
+
+  final String name;
+  //final double quantity;
+  //final String unit;
+}
--- lib/screen/home/fridge_page.dart
+++ lib/screen/home/fridge_page.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:foodcoin/model/food_item.dart';
+import 'package:foodcoin/repository/database/firebase_database_repository.dart';
 import '../../bloc/authentication/authentication_bloc.dart';
 import '../../bloc/authentication/authentication_state.dart';
 
@@ -14,22 +16,26 @@ class FridgePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<AuthenticationBloc, AuthenticationState>(
       builder: (context, state) {
-        User user;
+        FirebaseDatabaseRepository db;
 
         if (state is UserAuthenticated) {
           print(state.user);
-          user = state.user;
+          db = FirebaseDatabaseRepository(user: state.user);
         } else {
           throw Error();
         }
 
-        return FutureBuilder<QuerySnapshot>(
-          future: FirebaseFirestore.instance.collection(user.uid).get(),
+        return FutureBuilder<Iterable<FoodItem>>(
+          future: db.getFoodItems(),
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              return Column(
-                  children: snapshot.data.docs
-                      .map<Widget>((x) => Text(x.get('text').toString()))
+              return ListView(
+                  children: snapshot.data
+                      .map<Widget>(
+                        (x) => ListTile(
+                          title: Text(x.name),
+                        ),
+                      )
                       .toList());
             } else {
               return const Center(child: CircularProgressIndicator());
