62c4e1cd5b810d8f45e0198c953331ebf047403c
Zoey Child <imperialoctopus@gmail.com>
2021-01-24 16:04:29 +0000

    Add top app bar to home

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -61,6 +61,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: SafeArea(
         child: PageView(
           controller: _pageController,
