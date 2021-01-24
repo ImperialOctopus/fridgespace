4990edd128f33644b43d22459638eeb3c278a626
OliverWales <oliverfwales@gmail.com>
2021-01-24 12:24:46 +0000

    Add offer model

---
+++ lib/model/offer.dart
@@ -0,0 +1,24 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+
+import 'food_item.dart';
+
+/// Model of a bubble of users.
+class Offer {
+  /// Name of person offering.
+  final String offerer;
+
+  /// Name of person offering.
+  final Url profileImage;
+
+  /// Food item on offer.
+  final FoodItem foodItem;
+
+  /// Model of an item of food offered to the bubble.
+  const Offer({
+    @required this.offerer,
+    @required this.profileImage,
+    @required this.foodItem,
+  });
+}
