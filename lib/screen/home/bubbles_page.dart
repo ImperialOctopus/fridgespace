688b67c1258cef8281493feb80a557b28dc48ec2
Jenny <jenny.lynn.fletcher@gmail.com>
2021-01-24 15:03:39 +0000

    Add create bubble button

--- lib/screen/home/bubbles_page.dart
+++ lib/screen/home/bubbles_page.dart
@@ -19,42 +19,49 @@ class BubblesPage extends StatelessWidget {
             body:
             ListView.separated(
               padding: const EdgeInsets.all(8),
-              itemCount: list.length,
+              itemCount: list.length + 1,
               itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  height: 50,
-                  child: Card(
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 10.0),
-                                child:
-                                Text('Bubble: ${list
-                                    .elementAt(index)
-                                    .name}')),
-                            const Padding(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 10.0, vertical: 4.0),
-                                child:
-                                RaisedButton(
-                                    onPressed: null,
-                                    child: Text('Leave Bubble')
-                                )
-                            )
-                          ]
-                      )
-                  ),
-                );
+                if (index < list.length) {
+                  return Container(
+                    height: 50,
+                    child: Card(
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10.0),
+                                  child:
+                                  Text('Bubble: ${list
+                                      .elementAt(index)
+                                      .name}')),
+                              const Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 10.0, vertical: 4.0),
+                                  child:
+                                  RaisedButton(
+                                      onPressed: null,
+                                      child: Text('Leave Bubble')
+                                  )
+                              )
+                            ]
+                        )
+                    ),
+                  );
+                } else {
+                  return const FlatButton(onPressed: null, child: Text('Create Bubble'));
+                }
               },
               separatorBuilder: (BuildContext context, int index) =>
               const Divider(),
             ),
           );
         } else {
-          return const Center(
-              child: Text("You aren't part of any bubbles yet")
+          return Column(
+            children: <Widget>[
+              const Text("You aren't part of any bubbles yet"),
+              const FlatButton(onPressed: null, child: Text('Create Bubble'))
+            ]
           );
         }
       } else if (state is BubbleError) {
