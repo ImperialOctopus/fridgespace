2f512be20800ba3a7383453c73466f304fce4ecd
Zoey Child <imperialoctopus@gmail.com>
2021-01-24 06:21:06 +0000

    Add bubble page

---
+++ lib/screen/home/bubbles_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import '../../model/bubble.dart';
+
+class BubblesPage extends StatelessWidget {
+  final list = <Bubble>[];
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
