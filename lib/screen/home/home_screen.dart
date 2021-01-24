7242c46f148edc43cc40ad55114a4926ea85fa30
Zoey Child <imperialoctopus@gmail.com>
2021-01-24 00:42:50 +0000

    Add swipey pages to home screen

--- lib/screen/home/home_screen.dart
+++ lib/screen/home/home_screen.dart
@@ -35,10 +35,31 @@ class _HomeScreenPages extends StatefulWidget {
 class _HomeScreenState extends State<_HomeScreenPages> {
   int _pageIndex;
 
+  final PageController _pageController = PageController(
+    initialPage: 0,
+    keepPage: true,
+  );
+
   static const List<Widget> _pages = <Widget>[
     QrPage(),
     FridgePage(),
     SearchPage(),
+  ];
+
+  static const List<BottomNavigationBarItem> _bottomNavigationBarItems =
+      <BottomNavigationBarItem>[
+    BottomNavigationBarItem(
+      icon: Icon(Icons.bubble_chart),
+      label: 'QR',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.business),
+      label: 'Fridge',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.school),
+      label: 'Search',
+    ),
   ];
 
   @override
@@ -47,35 +68,34 @@ class _HomeScreenState extends State<_HomeScreenPages> {
     _pageIndex = 0;
   }
 
-  void _onItemTapped(int index) {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          _onPageChanged(index);
+        },
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _pageIndex,
+        items: _bottomNavigationBarItems,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  void _onPageChanged(int index) {
     setState(() {
       _pageIndex = index;
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[_pageIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'QR',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Fridge',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'Search',
-          ),
-        ],
-        currentIndex: _pageIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
-    );
+  void _onItemTapped(int index) {
+    setState(() {
+      _pageController.animateToPage(index,
+          duration: const Duration(milliseconds: 200), curve: Curves.easeInOut);
+    });
   }
 }
