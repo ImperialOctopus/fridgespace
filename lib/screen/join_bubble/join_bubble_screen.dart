613121719ede8a8952df6e34ac05032a2dd5b47a
Zoey Child <imperialoctopus@gmail.com>
2021-01-24 11:12:09 +0000

    Add joining bubbles

--- lib/screen/join_bubble/join_bubble_screen.dart
+++ lib/screen/join_bubble/join_bubble_screen.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../bloc/bubble/bubble_bloc.dart';
-import '../../bloc/bubble/bubble_event.dart';
+import '../../exception/join_bubble_exception.dart';
 import '../../extension/uppercase_text_formatter.dart';
+import '../../service/bubble_join_service.dart';
 
 /// Page to join a bubble.
 class JoinBubbleScreen extends StatefulWidget {
@@ -18,6 +18,8 @@ class JoinBubbleScreen extends StatefulWidget {
 class _JoinBubbleScreenState extends State<JoinBubbleScreen> {
   final _textController = TextEditingController();
 
+  bool loading = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,34 +29,61 @@ class _JoinBubbleScreenState extends State<JoinBubbleScreen> {
           onPressed: () => Navigator.of(context).pop(),
         ),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            const Text('Join a Bubble'),
-            TextField(
-              controller: _textController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: const BorderSide(),
-                ),
+      body: (loading)
+          ? const Center(child: CircularProgressIndicator())
+          : Center(
+              child: Column(
+                children: [
+                  const Text('Join a Bubble'),
+                  TextField(
+                    controller: _textController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: const BorderSide(),
+                      ),
+                    ),
+                    textAlign: TextAlign.center,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.allow(RegExp('[a-zA-Z]')),
+                      UpperCaseTextFormatter(),
+                    ],
+                    maxLength: 5,
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                  ElevatedButton(
+                      child: const Text('Join!'),
+                      onPressed: () async {
+                        final future =
+                            RepositoryProvider.of<BubbleJoinService>(context)
+                                .joinBubble(_textController.text);
+                        setState(() {
+                          loading = true;
+                        });
+                        try {
+                          await future;
+
+                          /// Pop success message.
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content:
+                                    Text('Successfully joined a new bubble!')),
+                          );
+                          Navigator.of(context).pop();
+                        } on JoinBubbleException catch (e) {
+                          loading = false;
+
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Failed to join bubble!\nError: ' +
+                                  e.message),
+                            ),
+                          );
+                        }
+                      })
+                ],
               ),
-              textAlign: TextAlign.center,
-              inputFormatters: [
-                FilteringTextInputFormatter.allow(RegExp('[a-zA-Z]')),
-                UpperCaseTextFormatter(),
-              ],
-              maxLength: 5,
-              style: Theme.of(context).textTheme.headline4,
             ),
-            ElevatedButton(
-              child: const Text('Join!'),
-              onPressed: () => BlocProvider.of<BubbleBloc>(context)
-                  .add(JoinBubble(code: _textController.text)),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ lib/service/bubble_join_service.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../model/bubble.dart';
+import '../repository/database/database_repository.dart';
+
+/// Service to join or leave bubbles.
+class BubbleJoinService {
+  final DatabaseRepository _databaseRepository;
+
+  /// Service to join or leave bubbles.
+  const BubbleJoinService({@required DatabaseRepository databaseRepository})
+      : _databaseRepository = databaseRepository;
+
+  /// Join a bubble by code.
+  Future<void> joinBubble(String code) => _databaseRepository.joinBubble(code);
+
+  /// Leave a bubble.
+  Future<void> leaveBubble(Bubble bubble) {
+    // TODO: implement leave bubble
+    throw UnimplementedError();
+  }
+}
