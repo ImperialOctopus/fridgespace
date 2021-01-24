a66958e90cc3daafb62da3d6be0f7f46d110f427
Zoey Child <imperialoctopus@gmail.com>
2021-01-24 05:21:04 +0000

    Reorganise form

--- lib/screen/add_item/add_item_screen.dart
+++ lib/screen/add_item/add_item_screen.dart
@@ -1,37 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-
+import '../../model/food_item.dart';
 import '../../model/product_lookup_result.dart';
 
-class ItemForm extends StatefulWidget {
+/// Screen for adding a new item.
+class AddItemScreen extends StatefulWidget {
+  /// Result of attempting to lookup this product.
+  final ProductLookupResult lookupResult; // Nullable
+
+  /// Screen for adding a new item.
+  const AddItemScreen({@required this.lookupResult});
+
   @override
-  ItemFormState createState() {
-    return ItemFormState();
-  }
+  State<StatefulWidget> createState() => _AddItemScreenState();
 }
 
-class ItemFormState extends State<ItemForm> {
-  // Create a global key that uniquely identifies the Form widget
-  // and allows validation of the form.
-  //
-  // Note: This is a `GlobalKey<FormState>`,
-  // not a GlobalKey<MyCustomFormState>.
+class _AddItemScreenState extends State<AddItemScreen> {
   final _formKey = GlobalKey<FormState>();
+
+  TextEditingController _nameController;
+  TextEditingController _quantityController;
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController();
+    _quantityController = TextEditingController();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          FlatButton(
+            child: const Text(
+              'SAVE',
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: _submitForm,
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: form(),
+        ),
+      ),
+    );
+  }
+
+  Widget form() {
     return Form(
       key: _formKey,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           TextFormField(
+            controller: _nameController,
             decoration: const InputDecoration(
-                border: InputBorder.none,
-                labelText: 'Item Name'
-            ),
+                border: InputBorder.none, labelText: 'Item Name'),
             validator: (value) {
               if (value.isEmpty) {
                 return 'Please enter some text';
@@ -39,14 +69,12 @@ class ItemFormState extends State<ItemForm> {
               return null;
             },
           ),
-
           TextFormField(
+            controller: _quantityController,
             keyboardType: TextInputType.number,
             inputFormatters: [FilteringTextInputFormatter.digitsOnly],
             decoration: const InputDecoration(
-                border: InputBorder.none,
-                labelText: 'Item Weight/Count'
-            ),
+                border: InputBorder.none, labelText: 'Item Weight/Count'),
             validator: (value) {
               if (value.isEmpty) {
                 return 'Please enter a value';
@@ -54,12 +82,9 @@ class ItemFormState extends State<ItemForm> {
               return null;
             },
           ),
-
           TextFormField(
             decoration: const InputDecoration(
-                border: InputBorder.none,
-                labelText: 'Expiry Date'
-            ),
+                border: InputBorder.none, labelText: 'Expiry Date'),
             validator: (value) {
               if (value.isEmpty) {
                 return 'Please enter a value';
@@ -70,50 +95,28 @@ class ItemFormState extends State<ItemForm> {
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 10.0),
             child: ElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState.validate()) {
-                  Scaffold.of(context)
-                      .showSnackBar(SnackBar(content: Text('Entering Item')));
-                }
-              },
-              child: Text('Submit'),
+              onPressed: _submitForm,
+              child: const Text('Submit'),
             ),
           ),
         ],
       ),
     );
   }
-}
 
-/// Screen for adding a new item.
-class AddItemScreen extends StatelessWidget {
-  /// Result of attempting to lookup this product.
-  final ProductLookupResult lookupResult; ///could be null
+  void _submitForm() {
+    if (_formKey.currentState.validate()) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text('Entering Item')));
 
-  /// Screen for adding a new item.
-  const AddItemScreen({@required this.lookupResult});
+      final item = FoodItem(
+        name: _nameController.text,
+        quantity: _quantityController.text,
+        expires: null,
+        shared: null,
+      );
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          FlatButton(
-            child: const Text('SAVE'),
-            onPressed: () {},
-          )
-        ],
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            children: [
-              ItemForm()
-            ],
-          ),
-        ),
-      ),
-    );
+      print(item.toJson());
+    }
   }
 }
