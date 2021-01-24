eb1001e35c2d816a290280558b1126cc24d46da4
Zoey Child <imperialoctopus@gmail.com>
2021-01-24 03:38:20 +0000

    Add lookup result information to add item screen

--- lib/screen/add_item/add_item_screen.dart
+++ lib/screen/add_item/add_item_screen.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 
+import '../../model/product_lookup_result.dart';
+
 /// Screen for adding a new item.
 class AddItemScreen extends StatelessWidget {
+  /// Result of attempting to lookup this product.
+  final ProductLookupResult lookupResult;
+
+  /// Screen for adding a new item.
+  const AddItemScreen({@required this.lookupResult});
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Add Item Screen'),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Text('Add Item Screen'),
+            Text(lookupResult.toString()),
+            Text(lookupResult?.name ?? 'name missing'),
+            Text(lookupResult?.quantity ?? 'quantity missing'),
+          ],
+        ),
       ),
     );
   }
